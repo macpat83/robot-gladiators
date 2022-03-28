7108b22e2134342cacaf62d0fe4e44ef458a0d68
macpat83 <macpat@gmail.com>
2022-03-28 15:32:14 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,17 +73,53 @@ var fight = function(enemyName) {
     } // end of while loop
   }; // end of fight function
 
-// run fight function to start game
-for(var i = 0; i < enemyNames.length; i++) {
+// function to start a new game
+var startGame = function() {
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+  for(var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
         window.alert("Welcome to Robot Gladiators! Round " + ( i + 1));
+        var pickedEnemyName = enemyNames[i];
+
+        enemyHealth = 50;
+
+        fight(pickedEnemyName);
+
       }
+
       else {
           window.alert("You have lost your robot in battle! Game Over!")
           break;
       }
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+  }
+  //play again
+  endGame();
+};
+
+var endGame = function() {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job you've survived the game! You now have a score of " + playerMoney + ".");
+  }
+  else {
+  window.alert("You've lost your robot in battle.");
+
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  //restart the game
+  startGame();
 }
+else {
+  window.alert("Thank you for playing Robot Gladiatores! Come back soon!");
+}
+  }
+}
+startGame();
+
+
 
